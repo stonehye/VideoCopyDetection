34ba90fadd116c943bb4662c74dad70857e27978
stonehye <stonehye@gmail.com>
2020-11-20 03:35:26 +0900

    BOW encoding 최적화

--- C_BOW_encoding.py
+++ C_BOW_encoding.py
@@ -8,12 +8,12 @@ import argparse
 parser = argparse.ArgumentParser(description='C. BOW encoding')
 
 parser.add_argument('--n_clust', required=False, default=20000, help="# of cluster centroid")
-parser.add_argument('--model_path', required=True, default='model-mul-vcdb_core-1fps-MobileNet_triplet_sum-5sec-20000clusters.pkl')
+parser.add_argument('--model_path', required=True, default='/nfs_shared_/hkseok/cluster/model-mul-vcdb_core_1fps-Mobilenet_localsum-5sec-20000clusters.pkl')
 parser.add_argument('--feature_path', required=True,
-                        default='/nfs_shared_/hkseok/features_local/multiple/vcdb_core-1fps-MobileNet_triplet_sum-5sec',
+                        default='/nfs_shared_/hkseok/features_local/multiple/nsfw-1fps-mobilenet-5sec',
                         help="source feature path")
 parser.add_argument('--BOW_path', required=True,
-                        default='/nfs_shared_/hkseok/BOW/multiple/vcdb_core-1fps-MobileNet_triplet_sum-5sec',
+                        default='/nfs_shared_/hkseok/BOW/multiple/nsfw-1fps-mobilenet-5sec',
                         help="dst feature path")
 
 args = parser.parse_args()
@@ -21,8 +21,9 @@ print(args)
 
 N_cluster = int(args.n_clust)
 kmeans = pickle.load(open(args.model_path, "rb"))
+kmeans.verbose = False
 feature_list = glob.glob(os.path.join(args.feature_path, '*'))
-scaler = MinMaxScaler()
+# scaler = MinMaxScaler()
 pth_dir = args.BOW_path
 if not os.path.isdir(pth_dir):
     os.makedirs(pth_dir)
@@ -37,13 +38,16 @@ for idx, feature in enumerate(feature_list):
         x = torch.cat(frame).numpy()
         predict = kmeans.predict(x)
 
-        # BOW encoding
-        frame_vector = np.array([np.count_nonzero(predict == i) for i in range(N_cluster)]).astype(np.float32)
+        unique, counts = np.unique(predict, return_counts=True)
+        frame_vector = [0] * N_cluster
+        for uniq, cnts in zip(unique, counts):
+            frame_vector[uniq] = cnts
+        frame_vector = np.array(frame_vector).astype(np.float32)
 
         # minmax scalar
-        scaler.fit(frame_vector.reshape(-1,1))
-        frame_vector = scaler.transform(frame_vector.reshape(-1,1))
-        frame_vector = frame_vector.squeeze(-1)
+        # scaler.fit(frame_vector.reshape(-1,1))
+        # frame_vector = scaler.transform(frame_vector.reshape(-1,1))
+        # frame_vector = frame_vector.squeeze(-1)
 
         video_vector.append(torch.from_numpy(frame_vector))
 
